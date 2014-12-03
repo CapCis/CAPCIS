6de57e7a290dc1a0ec4fe1d3d4ce6f9460d00d8a
CapCis <talevey@courtap.com>
2014-12-02 18:06:25 -0600

    Update #37

---
+++ CAPCIS/WebFolder/CAPCIS.waPage/scripts/index.js
@@ -0,0 +1,25 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		WAF.loadComponent ( {	
+		id:   	'component1',
+		path: 	'/CAPCIS.waPage/capcisLogin.waComponent',
+		onSuccess: function () {
+			$$('textField1').setValue("my username")//do something here
+			
+		}
+		});
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
