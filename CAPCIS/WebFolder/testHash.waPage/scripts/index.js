55771d658ab0a442251e913e2fbd4683cf1833c0
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-12-07 13:35:35 -0600

    Update #47

---
+++ CAPCIS/WebFolder/testHash.waPage/scripts/index.js
@@ -0,0 +1,22 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var genHash = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	genHash.click = function genHash_click (event)// @startlock
+	{// @endlock
+		$$("richText2").setValue($$("textUser").getValue());
+		var user = $$("textUser").getValue();
+		var pass = $$("textPass").getValue();
+		var hash = rpcUtilities.getHash(user,pass);
+		$$("hashOut").setValue(hash);
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("genHash", "click", genHash.click, "WAF");
+// @endregion
+};// @endlock
