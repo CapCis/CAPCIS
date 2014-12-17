6a3cb63689ddffe8209c94bb8a11c6950edaf8e6
CapCis <talevey@courtap.com>
2014-12-17 17:18:16 -0600

    Update #76

--- CAPCIS/Model/Clientpictures/Clientpictures-methods.js
+++ CAPCIS/Model/Clientpictures/Clientpictures-methods.js
@@ -1,23 +1,46 @@
 ﻿
 
-model.Clientpictures.methods.getclientpictures = function() {
-	var serverUtil = require('serverUtilities');
-	var connection = serverUtil.getDBConnection();
-	
-	var deleteOld = ds.Clientpictures.query("ClientPictureID < :1",10);
-	deleteOld.remove();
-	
-	var dbQuery = 'SELECT ClientPictureID,ClientPicture FROM clientpicture WHERE ClientPictureID < 10';
-	var result = connection.execute(dbQuery);	
-	while (result.hasNext())
+model.Clientpictures.methods.getclientpictures = function(methodResponse) {
+	try
 	{
-		var thisRow = result.getNextRow();
-		var clientPic = ds.Clientpictures.createEntity();
-		clientPic.ClientPictureID = thisRow.ClientPictureID;
-		clientPic.ClientPicture = thisRow.ClientPicture;
-		clientPic.save();
-		
+		var serverUtil = require('serverUtilities');
+		var connection = serverUtil.getDBConnection();
 	}
-	connection.close();
+	catch(err)
+	{
+		return("error code 9000, could not connect to database");	
+	}
+	
+	try
+	{
+		var deleteOld = ds.Clientpictures.query("ClientPictureID < :1",10); 										//get all records (entities) into deleteOld
+		deleteOld.remove(); 																						//delete all records (entities) deleteOld represents
+	}
+	catch(err)
+	{
+		return("error code 9001, data error")	
+	}
+	
+	//try
+	//{
+		var dbQuery = 'SELECT ClientPictureID,ClientPicture FROM clientpicture WHERE ClientPictureID < 10';
+		var result = connection.execute(dbQuery);	
+		while (result.hasaNext())
+		{
+			var thisRow = result.getNextRow();
+			var clientPic = ds.Clientpictures.createEntity();
+			clientPic.ClientPictureID = thisRow.ClientPictureID;
+			clientPic.ClientPicture = thisRow.ClientPicture;
+			clientPic.save();		
+		}
+		connection.close();
+		methodResponse = "success";
+		return(methodResponse);
+	//}
+	//catch(err)
+	//{
+		var parsedErr = err.message.toString();
+		return("error code 9002, picture error");
+	//}
 };
 model.Clientpictures.methods.getclientpictures.scope = 'public';
--- CAPCIS/WebFolder/timsTest.waPage/scripts/index.js
+++ CAPCIS/WebFolder/timsTest.waPage/scripts/index.js
@@ -9,11 +9,28 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 	button1.click = function button1_click (event)// @startlock
 	{// @endlock
-		debugger;		
-		ds.Clientpictures.getclientpictures();				
-		source.clientpictures.orderBy("ClientPicturesID desc");
-		var sec;
-		//sources.clientpictures.all();		
+		debugger;
+		var myResponse = ds.Clientpictures.getclientpictures(
+			{
+				onSuccess: handleOnReturnSuccess,
+				onError: handleOnReturnError
+			});
+		
+		function handleOnReturnSuccess(event)
+			{
+				source.clientpictures.orderBy("ClientPicturesID desc");
+				var myEvent = event
+			}
+		function handleOnReturnError(event)
+			{
+				var errMessage;
+				for (var x = 0;x < event.error.length;x++)
+				{
+					errMessage += (event.error[x].message + ",");
+				}
+				alert(errMessage);			
+			};
+		//sources.clientpictures.all();
 	};// @lock
 
 // @region eventManager// @startlock
