527a877c36186e2a0bedddee21a8e91aa7ff3059
mlevey00 <mlevey@me.com>
2014-12-28 19:32:42 -0600

    Update #137

--- CAPCIS/WebFolder/MattsWidgetTester.waPage/scripts/index.js
+++ CAPCIS/WebFolder/MattsWidgetTester.waPage/scripts/index.js
@@ -2,10 +2,24 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var button16 = {};	// @button
+	var button15 = {};	// @button
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	button16.click = function button16_click (event)// @startlock
+	{// @endlock
+		$$('dialog2').closeDialog(); //ok button
+	};// @lock
+
+	button15.click = function button15_click (event)// @startlock
+	{// @endlock
+		$$('dialog2').closeDialog(); //cancel button
+	};// @lock
+
 // @region eventManager// @startlock
+	WAF.addListener("button16", "click", button16.click, "WAF");
+	WAF.addListener("button15", "click", button15.click, "WAF");
 // @endregion
 };// @endlock
