2c83ec1dfcb4c7d16392fcd12070a7db574bf9bb
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-01-10 23:54:04 -0600

    Update #158

--- CAPCIS/WebFolder/CAPCIS.waPage/capcisLogin.waComponent/capcisLogin.js
+++ CAPCIS/WebFolder/CAPCIS.waPage/capcisLogin.waComponent/capcisLogin.js
@@ -11,52 +11,80 @@ function constructor (id) {
 	// @endregion// @endlock
 
 	this.load = function (data) {// @lock
-		
-		 //(data.userData.myTestValue);
+		debugger;
+		$$(getHtmlId("tfUserName")).focus();
+	//(data.userData.myTestValue);
 	// @region namespaceDeclaration// @startlock
-	var button4 = {};	// @button
+	var tfPassword = {};	// @textField
 	var button1 = {};	// @button
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
-	button4.click = function button4_click (event)// @startlock
+	tfPassword.keydown = function tfPassword_keydown (event)// @startlock
 	{// @endlock
-		debugger;
-		var myVa = testGlobal;
+		if(event.keyCode === 13) 
+		{
+			loginGo();			
+		}
 		
 	};// @lock
 
 	button1.click = function button1_click (event)// @startlock
 	{// @endlock
-		debugger;
+		loginGo();
+	};// @lock
+	
+	function loginGo()
+	{
 		$$(getHtmlId("rtMessage")).setValue("");
 		var myPassword = $$(getHtmlId("tfPassword")).getValue();
         var passHash = CryptoJS.SHA3(myPassword).toString();   //Hash password on client        
-		dsLogin = rpcGetToken.getToken(myArray = [passHash,0,0,$$(getHtmlId("tfUserName")).getValue()]);		
+		dsLogin = rpcGetToken.getToken(myArray = {data1:passHash,major:0,minor:0,user:$$(getHtmlId("tfUserName")).getValue()});		
 		sources.dsLogin.sync();	
 		
 		
-		if (dsLogin.length > 0)
+		if (dsLogin.length !== 2)
 		{
-			if(dsLogin[0] == "err")
+			if(dsLogin.myToken === "err")
 			{
 				$$(getHtmlId("rtMessage")).setValue("Invalid User Name Or Password");
 			}
 			else 
 			{
-				//close this popup
-				//open main forms
+				$$("capcisSignInOut").setValue("Sign Out");			
+				userConfigObj.fullName = dsLogin[0].FullName;
+				userConfigObj.userName = $$(getHtmlId("tfUserName")).getValue();
+				userConfigObj.secToken = dsLogin[0].myToken;
+				userConfigObj.userID = dsLogin[0].FxUserAccountsID;
+				userConfigObj.selectedUserDivision = dsLogin[0].myDivisionResults[0].FK_DivisionInformationID;
+				$$("capcisUserGreeting").setValue("Hello, "+userConfigObj.fullName);				
+				for (var x = 0;x < dsLogin[0].myDivisionResults.length;x++)
+				{
+					userConfigObj.approvedDivArray[x] = dsLogin[0].myDivisionResults[x].FK_DivisionInformationID;
+				}
+				$$("capcisLoginWC").removeComponent();
+				$$("capcisSignInOut").toggle("hidden");
+				WAF.loadComponent ( {											//load webcomponent into this page component1 element
+				id: 	'capcisMainSelect', 											//designate the component to load into
+				path: 	'/CAPCIS.waPage/capcisMainSelect.waComponent', 				//designate the webcomponent to load
+				userData: 	{myTestData: 'I made it'}							//data that you want to send to the webcomponent
+				});
+				
+				WAF.loadComponent ( {											//load webcomponent into this page component1 element
+				id: 	'capcisMainWC', 											//designate the component to load into
+				path: 	'/CAPCIS.waPage/capcisPrimary.waComponent'				//designate the webcomponent to load
+				});
+				
 			}
 		}
 		else
 		{
 			$$(getHtmlId("rtMessage")).setValue("Invalid User Name Or Password");
 		}
-	};// @lock
-
+	}
 	// @region eventManager// @startlock
-	WAF.addListener(this.id + "_button4", "click", button4.click, "WAF");
+	WAF.addListener(this.id + "_tfPassword", "keydown", tfPassword.keydown, "WAF");
 	WAF.addListener(this.id + "_button1", "click", button1.click, "WAF");
 	// @endregion// @endlock
 	//$$(getHtmlId('textField2')).setValue(data.myData);
