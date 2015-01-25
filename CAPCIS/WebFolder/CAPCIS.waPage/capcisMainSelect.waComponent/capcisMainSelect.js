68d0996bd6efbfc0484bb9690a353b81d4831b47
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-01-25 00:45:11 -0600

    Update #205

--- CAPCIS/WebFolder/CAPCIS.waPage/capcisMainSelect.waComponent/capcisMainSelect.js
+++ CAPCIS/WebFolder/CAPCIS.waPage/capcisMainSelect.waComponent/capcisMainSelect.js
@@ -102,25 +102,32 @@ function constructor (id) {
 	    var subColumnNumber = openOrFindSubColumn(mainColumnNumber);  //go find an open sub column
 	    
 	    var getMainTabButton = document.getElementById("capcisMainWC_mt" + mainTabNumber.toString());   //set the main button config
-	    getMainTabButton.style.top = "250px";
+	    getMainTabButton.style.top = "0px";
 	    getMainTabButton.style.left = mainColumnPosLeft[mainColumnNumber] + "px";
 	    getMainTabButton.style.visibility = "visible";
 	    getMainTabButton.innerText = id.innerText;
 	    tabColumnTracking[mainColumnNumber].mainColumnButtonID = "capcisMainWC_mt" + mainTabNumber.toString();  //set the button name in the column tracker
+	    
+	    var getMainTabContainer = document.getElementById("capcisMainWC_mc" + mainTabNumber.toString()); //set the main container visible and current active
+	    //$$("capcisMainWC_mc" + mainTabNumber.toString()).show();
+	    getMainTabContainer.style.display = "block";
+	    currentVisibleMainContainer = "capcisMainWC_mc" + mainTabNumber.toString();
 	    	    
 	    var getMainTabX = document.getElementById("capcisMainWC_mtc" + mainTabNumber.toString());     //set the main X button config
-	    getMainTabX.style.top = "250px";
+	    getMainTabX.style.top = "0px";
 	    getMainTabX.style.left = mainColumnPosLeft[mainColumnNumber] + "px";
 	    getMainTabX.style.visibility = "visible";
 	    
 	    var getSubTabButton = document.getElementById("capcisMainWC_st" + mainTabNumber.toString() + subTabNumber.toString());  //set the subtab config
+	    getSubTabButton.style.display = "block";
 	    getSubTabButton.style.top = subColumnPosTop[subColumnNumber] + "px";
-	    getSubTabButton.style.left = "77px";
+	    getSubTabButton.style.left = "0px";
 	    tabColumnTracking[mainColumnNumber].subColumnButtonID[subColumnNumber] = "capcisMainWC_st" + mainTabNumber.toString() + subTabNumber.toString();
 	    
-	    var getSubTabX = document.getElementById("capcisMainWC_stc" + mainTabNumber.toString() + subTabNumber.toString());
+	    var getSubTabX = document.getElementById("capcisMainWC_stc" + mainTabNumber.toString() + subTabNumber.toString());   //set the subtab X button config
+	    getSubTabX.style.display = "block";
 	    getSubTabX.style.top = subColumnPosTop[subColumnNumber] + "px";
-	    getSubTabX.style.left = "77px";
+	    getSubTabX.style.left = "0px";
 	    
 	    
 	    
--- CAPCIS/WebFolder/CAPCIS.waPage/scripts/index.js
+++ CAPCIS/WebFolder/CAPCIS.waPage/scripts/index.js
@@ -30,10 +30,10 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	{// @endlock
 		
 		userConfigObj = {fullName:"",userName:"",userID:"",secToken:"",selectedUserDivision:"",approvedDivArray:[]};
-		currentVisibleMainTab = "";
+		currentVisibleMainContainer = "";
 		currentVisibleSubTab = "";
 		mainColumnPosLeft = [0,126,251,376,501,626,751,876,1001,1126];
-		subColumnPosTop = [300,400,500,600,700,800,900,700,800,900];
+		subColumnPosTop = [36,60,75,90,105,120,135,145,165,180];
 		tabColumnTracking = [																	//array to track used columns
 			tabsPos = {
 				mainColumnOpen:"",
