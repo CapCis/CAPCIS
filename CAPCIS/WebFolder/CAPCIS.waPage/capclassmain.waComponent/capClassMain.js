cb9089639e2f8e93c09dde72ab8929e5f76b3cea
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-01-31 21:15:44 -0600

    Update #220

--- CAPCIS/WebFolder/CAPCIS.waPage/capclassmain.waComponent/capClassMain.js
+++ CAPCIS/WebFolder/CAPCIS.waPage/capclassmain.waComponent/capClassMain.js
@@ -12,13 +12,16 @@ function constructor (id) {
 
 	this.load = function (data) {// @lock
 		debugger;
-		var full = document.getElementById(getHtmlId('classFullViewCont'));
-		var filter = document.getElementById(getHtmlId('classFilterViewCont'));
-		full.style.visibility = 'Visible';
-		filter.sytle.visibility = 'Hidden';
+
+		var whatsVisible = 'full'
+		fillFullView();
+		var myObject = {token:'7836140170460568' ,id:'1',major:2,minor:1}; //dontforget to add this to token userConfigObj.secToken  userConfigObj.userID
+	 	myPrograms = rpcDSelects.getSelect(myObject);
+	 	var filterMatrix = document.getElementById(getHtmlId('matrix4'));
+	 	filterMatrix.style.display = 'none';
+	 	sources.myPrograms.sync();
 	// @region namespaceDeclaration// @startlock
 	var filterButton = {};	// @button
-	var matrix5 = {};	// @matrix
 	var button4 = {};	// @button
 	var fullButton = {};	// @button
 	// @endregion// @endlock
@@ -30,46 +33,40 @@ function constructor (id) {
 		debugger;
 		var filter = document.getElementById(getHtmlId('classFilterViewCont'));
 		var full = document.getElementById(getHtmlId('classFullViewCont'));
-		var ifHidden = filter.style.visibility;
-		if(ifHidden == 'visible')
+		var ifHidden = filter.style.visibility.toString();
+		if(whatsVisible == 'full')
 		{
-			
+			whatsVisible = 'filter';
+			filter.style.display = 'block';
+			full.style.display = 'none';
 		}
-		else
-		{
-			filter.style.visibility = 'Visible';
-			full.sytle.visibility = 'Hidden';
-		}
-			
-			
 		
-	};// @lock
-
-	matrix5.click = function matrix5_click (event)// @startlock
-	{// @endlock
-		var htmlID = $comp.id + "_classFilterMatrix";
-		var  selectedClass = $$(htmlID).getValue();
-		
+			
 		
 	};// @lock
 
 	button4.click = function button4_click (event)// @startlock
 	{// @endlock
+		debugger;
+		var full = document.getElementById(getHtmlId('classFullViewCont'));
+		var filter = document.getElementById(getHtmlId('classFilterViewCont'));
+		filterMatrix.style.display = 'block';
+		var day = $$(getHtmlId('combobox6')).getValue();
+		var program = $$(getHtmlId('combobox5')).getValue();
 		
-		var day = $$(getHtmlId('combobox2')).getValue();
-		var program = $$(getHtmlId('combobox1')).getValue();
+		
 		
 		if(day == "None" && program == "None")
 		{
-		/*
-		WAF.loadComponent ( {											//load webcomponent into this page component1 element
-		id: 	$comp.id, 											//designate the component to load into
-		path: 	'/CAPCIS.waPage/capClassMainSelectFullView.waComponent'	//designate the webcomponent to load
-		});
-		*/
+			whatsVisible = 'full';
+			filterMatrix.style.display = 'none';
+			full.style.display = 'block';
+			filter.style.display = 'none';
+		
 		}
 		else if(day == "None")
 		{
+			
 			var myObject3 = {token:'7836140170460568' ,id:'1',major:2,minor:3,data1:program}; //dontforget to add this to token userConfigObj.secToken  userConfigObj.userID
 	 		filterResults = rpcDSelects.getSelect(myObject3);
 	 		sources.filterResults.sync();
@@ -87,6 +84,7 @@ function constructor (id) {
 			var myObject5 = {token:'7836140170460568' ,id:'1',major:2,minor:2,data1:day,data2:program}; //dontforget to add this to token userConfigObj.secToken  userConfigObj.userID
 	 		filterResults = rpcDSelects.getSelect(myObject5);
 	 		sources.filterResults.sync();
+	 		
 			
 		}
 		
@@ -101,22 +99,260 @@ function constructor (id) {
 		var full = document.getElementById(getHtmlId('classFullViewCont'));
 		var filter = document.getElementById(getHtmlId('classFilterViewCont'));
 		var ifHidden = full.style.visibility;
-		if(ifHidden == 'visible')
+		if(whatsVisible = 'filter')
 		{
-			
+			whatsVisible = 'full';
+			full.style.display = 'block';
+			filter.style.display = 'none';
+			filterMatrix.style.display = 'none';
 		}
-		else
-		{
-			full.style.visibility = 'Visible';
-			filter.sytle.visibility = 'Hidden';
-		}			//designate the webcomponent to load	
-	
+		
 		
 	};// @lock
+	function pageOpenerMU(id)
+	 {
+	 	debugger;
+		alert("clicked" + id.id.toString());
+		var myObject = {token:'7836140170460568' ,id:'1',major:2,minor:6,data1:id.id.toString(),weekStartDate:'2014-3-16',weekEndDate:'2014-3-22'}; //dont forget to change token and id
+	 	myMURosterList = rpcDSelects.getSelect(myObject);
+	 	sources.myMURosterList.sync();
+	 	
+	 };
+	 
+	 function pageOpener (id) 
+	{
+	
+		debugger;
+		alert("clicked" + id.id.toString());
+		var myObject = {token:'7836140170460568' ,id:'1',major:2,minor:5,data1:id.id.toString()}; //dont forget to change token and id
+	 	myRosterList = rpcDSelects.getSelect(myObject);
+	 	sources.myRosterList.sync();
+	 	var x = ds.myRosterList.CoalescedName[0];
+	 	var y=7;
+	
+		
+	};
+	function fillFullView()
+	{
+		
+		var myObject = {token:'7836140170460568' ,id:'1',major:2,minor:0}; //dontforget to add this to token userConfigObj.secToken  userConfigObj.userID
+	 	myClassList = rpcDSelects.getSelect(myObject);
+	 	sources.myClassList.sync();	//get list of classes
+	 
+	 	var muNotDone = true;
+	 	var lastDayWritten = "";
+	 	var top =0;
+	 
+	 	for(var x = 0; x < myClassList.length ; x++) //run through list
+	 	{
+	 	
+	 	
+	 		if(myClassList[x].ClassType == "Class") //if this is a class entry
+	 		{
+	 			if(x===0)//this is the first class
+	 			{
+	 				var textInput1 =  document.createElement('richText');
+	 				textInput1.setAttribute('type','richText');
+	 				textInput1.setAttribute('id','Class2'); // ID that links to the widget constructor ID
+        			textInput1.setAttribute('style','width:60px;height:12px;left:0px;top:0px;position:absolute;font:12px sans-serif;color:rgba(0,27,90,100);font-weight:bold');
+        			textInput1.setAttribute('rel','capcis.css');
+        		
+        			textInput1.setAttribute('class','waf-widget waf-richText waf-state-default');
+        			document.body.appendChild(textInput1); //insert html tag
+        		
+        			var textField2 = new WAF.widget.RichText({
+            			'id': "Class2", // ID 
+            			'data-lib': 'WAF',
+            			'data-type': 'textField',                         
+            			'class': 'waf-widget waf-textField default inherited'
+            		});
+            	
+            		var testme = getHtmlId('classFullViewCont');
+            		var testicles = $comp.id;
+            		$$(getHtmlId('classFullViewCont')).addChild($$("Class2"));
+                             
+            		$$('Class2').setValue('Class');
+            		textField2.setWidth(100);
+            		textField2.setTop(top);
+            		textField2.setHeight(12);
+            		top = top + 14;
+	 			
+	 			}
+	 		
+	 				if(lastDayWritten != myClassList[x].ClassDay) //this is a new day 
+	 				{	
+	 					var textInput =  document.createElement('richText');
+	 					textInput.setAttribute('type','richText');
+						textInput.setAttribute('id',myClassList[x].ClassDay.toString()+ "class"); // ID that links to the widget constructor ID
+        				textInput.setAttribute('style','width:60px;height:12px;left:15px;top:0px;position:absolute;font:12px sans-serif;color:rgba(0,27,90,100);font-weight:bold');
+        				textInput.setAttribute('rel','capcis.css');
+        				//textInput.addEventListener('click', function(){pageOpener(document.getElementById(this.id))}, false);
+        				textInput.setAttribute('class','waf-widget waf-richText waf-state-default');
+        				document.body.appendChild(textInput); //insert html tag
+        			
+        				var textField2 = new WAF.widget.RichText({
+            				'id': myClassList[x].ClassDay.toString()+"class", // ID 
+            				'data-lib': 'WAF',
+            				'data-type': 'textField',
+            				//'background-color':'#E5E5E5', 
+            				//'data-theme':'#E5E5E5',                           
+            				'class': 'waf-widget waf-textField default inherited'
+            				//'style': 'top:500;position:relative'
+            			});
+            
+            			$$(getHtmlId('classFullViewCont')).addChild($$(myClassList[x].ClassDay.toString()+"class"));
+            
+            			$$(myClassList[x].ClassDay.toString()+"class").setValue(myClassList[x].ClassDay.toString());
+            			textField2.setWidth(100);
+            			textField2.setTop(top);
+            			textField2.setHeight(12);
+            			top = top + 14;
+	 					lastDayWritten = myClassList[x].ClassDay;
+	 				}
+	 				if(lastDayWritten == myClassList[x].ClassDay) // this is not a new day
+	 				{
+	 					var textInput2 =  document.createElement('richText');
+	 					textInput2.setAttribute('type','richText');
+						textInput2.setAttribute('id',myClassList[x].Class.toString()); // ID that links to the widget constructor ID
+        				textInput2.setAttribute('style','width:60px;height:12px;left:30px;top:0px;position:absolute;font:12px sans-serif;color:rgba(0,27,90,100);font-weight:bold');
+        				textInput2.setAttribute('rel','capcis.css');
+        				textInput2.addEventListener('click', function(){pageOpener(document.getElementById(this.id))}, false);
+        				textInput2.setAttribute('class','waf-widget waf-richText waf-state-default');
+        				document.body.appendChild(textInput2); //insert html tag
+        			
+        				var textField2 = new WAF.widget.RichText({
+            				'id': myClassList[x].Class.toString(), // ID 
+            				'data-lib': 'WAF',
+            				'data-type': 'textField',
+            				//'background-color':'#E5E5E5', 
+            				//'data-theme':'#E5E5E5',                           
+            				'class': 'waf-widget waf-textField default inherited'
+            				//'style': 'top:500;position:relative'
+            			});
+            
+            			$$(getHtmlId('classFullViewCont')).addChild($$(myClassList[x].Class.toString()));
+            	
+            			$$(myClassList[x].Class.toString()).setValue(myClassList[x].Class.toString());
+            			textField2.setWidth(100);
+            			textField2.setTop(top);
+            			textField2.setHeight(12);
+            			top = top + 14;
+	 					currentDay = myClassList[x].WeekDayNumber;	 			
+	 				}
+	 			
+	 		
+	 				if(myClassList[x+1].ClassType == "MU Class")
+	 				{
+	 					lastDayWritten= "";
+	 				}
+	 		
+	 		
+	 		}
+	 		else if(myClassList[x].ClassType == "MU Class") //this is a MU Class entry
+	 		{
+	 			if(muNotDone) //first MU class
+	 			{
+	 				muNotDone = false;
+	 				var textInput3 =  document.createElement('richText');
+	 				textInput3.setAttribute('type','richText');
+	 				textInput3.setAttribute('id','muClass2'); // ID that links to the widget constructor ID
+        			textInput3.setAttribute('style','width:60px;height:12px;left:0px;top:0px;position:absolute;font:12px sans-serif;color:rgba(0,27,90,100);font-weight:bold');
+        			textInput3.setAttribute('rel','capcis.css');
+        			textInput3.setAttribute('class','waf-widget waf-richText waf-state-default');
+        			document.body.appendChild(textInput3); //insert html tag
+        		
+        			var textField2 = new WAF.widget.RichText({
+           		 		'id': "muClass2", // ID 
+          	  			'data-lib': 'WAF',
+            			'data-type': 'textField',                        
+            			'class': 'waf-widget waf-textField default inherited'
+         		   	});
+            
+            		$$(getHtmlId('classFullViewCont')).addChild($$('muClass2'));
+            
+            		$$('muClass2').setValue('MU Class');
+            		textField2.setWidth(100);
+            		textField2.setTop(top);
+            		textField2.setHeight(12);
+            		top = top + 14;
+	 			
+	 			}
+	 		
+	 			if(lastDayWritten != myClassList[x].ClassDay) //this is a new day 
+	 			{
+	 				var textInput4 =  document.createElement('richText');
+	 				textInput4.setAttribute('type','richText');
+					textInput4.setAttribute('id',myClassList[x].ClassDay.toString()); // ID that links to the widget constructor ID
+        			textInput4.setAttribute('style','width:60px;height:12px;left:15px;top:0px;position:absolute;font:12px sans-serif;color:rgba(0,27,90,100);font-weight:bold');
+        			textInput4.setAttribute('rel','capcis.css');
+        			//textInput.addEventListener('click', function(){pageOpener(document.getElementById(this.id))}, false);
+        			textInput4.setAttribute('class','waf-widget waf-richText waf-state-default');
+        			document.body.appendChild(textInput4); //insert html tag
+        		
+        			var textField2 = new WAF.widget.RichText({
+            			'id': myClassList[x].ClassDay.toString(), // ID 
+            			'data-lib': 'WAF',
+            			'data-type': 'textField',
+            			//'background-color':'#E5E5E5', 
+            			//'data-theme':'#E5E5E5',                           
+            			'class': 'waf-widget waf-textField default inherited'
+            			//'style': 'top:500;position:relative'
+            		});
+            
+            		$$(getHtmlId('classFullViewCont')).addChild($$(myClassList[x].ClassDay.toString()));
+            
+            		$$(myClassList[x].ClassDay.toString()).setValue(myClassList[x].ClassDay.toString());
+            		textField2.setWidth(100);
+            		textField2.setTop(top);
+            		textField2.setHeight(12);
+            		top = top + 14;
+	 				lastDayWritten = myClassList[x].ClassDay;
+	 			}
+	 			if(lastDayWritten == myClassList[x].ClassDay)//not a new day
+	 			{
+	 				var textInput5 =  document.createElement('richText');
+	 				textInput5.setAttribute('type','richText');
+					textInput5.setAttribute('id',myClassList[x].Class.toString()); // ID that links to the widget constructor ID
+        			textInput5.setAttribute('style','width:60px;height:12px;left:30px;top:0px;position:absolute;font:12px sans-serif;color:rgba(0,27,90,100);font-weight:bold');
+        			textInput5.setAttribute('rel','capcis.css');
+        			textInput5.addEventListener('click', function(){pageOpenerMU(document.getElementById(this.id))}, false);
+        			textInput5.setAttribute('class','waf-widget waf-richText waf-state-default');
+        			document.body.appendChild(textInput5); //insert html tag
+        		
+        			var textField2 = new WAF.widget.RichText({
+            			'id': myClassList[x].Class.toString(), // ID 
+            			'data-lib': 'WAF',
+            			'data-type': 'textField',
+            			//'background-color':'#E5E5E5', 
+            			//'data-theme':'#E5E5E5',                           
+            			'class': 'waf-widget waf-textField default inherited'
+            			//'style': 'top:500;position:relative'
+            		});
+            
+            		$$(getHtmlId('classFullViewCont')).addChild($$(myClassList[x].Class.toString()));
+            
+            		$$(myClassList[x].Class.toString()).setValue(myClassList[x].Class.toString());
+            		textField2.setWidth(100);
+            		textField2.setTop(top);
+            		textField2.setHeight(12);
+            		top = top + 14;
+	 				currentDay = myClassList[x].WeekDayNumber;
+	 			
+	 			
+	 			}
+	 			
+	 		
+	 			
+	 		
+	 		}
+	 	
+	 	
+		}
+		
+	};
 
 	// @region eventManager// @startlock
 	WAF.addListener(this.id + "_filterButton", "click", filterButton.click, "WAF");
-	WAF.addListener(this.id + "_matrix5", "click", matrix5.click, "WAF");
 	WAF.addListener(this.id + "_button4", "click", button4.click, "WAF");
 	WAF.addListener(this.id + "_fullButton", "click", fullButton.click, "WAF");
 	// @endregion// @endlock
