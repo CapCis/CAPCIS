1b12ff8af3ffa900cf0de27303242bf1a72ec474
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-02-16 21:39:15 -0600

    Update #299

--- CAPCIS/Modules/dUpdateQuery.js
+++ CAPCIS/Modules/dUpdateQuery.js
@@ -19,6 +19,15 @@
 						break;
 				case 1: answer = 'UPDATE capcis.assessorcorrespondence SET VoidedAssessorCorrespondence = '+myObject.data2+' WHERE AssessorCorrespondenceID = "'+myObject.data3+'"';
 						break;
+				case 2: answer = 'UPDATE capcis.attorneycorrespondence SET VoidedAttorneyCorrespondence = '+myObject.data2+' WHERE AttorneyCorrespondenceID = "'+myObject.data3+'"';
+						break;
+				case 3: answer =  'UPDATE capcis.assessorinformation SET AssessorName = "'+myObject.data1+'", AssessorPhone = "'+myObject.data2+'", \
+				 		AssessorEmail="'+myObject.data3+'", AssessorFax = "'+myObject.data4+'", AssessorAddress ="'+myObject.data5+'", AssessorCity = "'+myObject.data6+'", \
+ 						AssessorState = "'+myObject.data7+'", AssessorZipCode = "'+myObject.data8+'", AssessorPhoneExt= "'+myObject.data9+'", \
+ 						AssessorMobilePhone = "'+myObject.data10+'", AssessorNotes = "'+myObject.data11+'", AssessorFullNameDisplay = "'+myObject.data12+'",\
+ 						AssPreferredReportingMethod = "'+myObject.data13+'", InactiveAssessorInfo = '+myObject.data15+', PublicDefender = '+myObject.data16+', AttorneyStaffInfo = "'+myObject.data17+'" \
+						WHERE AssessorInformationID="'+myObject.data14+'";';
+						break;
 				default: answer = null;	
 						break;
 			}
--- CAPCIS/WebFolder/CAPCIS.waPage/referralAttorneys.waComponent/referralAttorneys.js
+++ CAPCIS/WebFolder/CAPCIS.waPage/referralAttorneys.waComponent/referralAttorneys.js
@@ -125,6 +125,12 @@ function constructor (id) {
 			}	
 		}
 	// @region namespaceDeclaration// @startlock
+	var button14 = {};	// @button
+	var button15 = {};	// @button
+	var button3 = {};	// @button
+	var Close = {};	// @button
+	var dataGrid1 = {};	// @dataGrid
+	var button16 = {};	// @button
 	var attVoidedCorrespondanceCheck = {};	// @checkbox
 	var button2 = {};	// @button
 	var attCorCloseButton = {};	// @button
@@ -135,18 +141,157 @@ function constructor (id) {
 
 	// eventHandlers// @lock
 
+	button14.click = function button14_click (event)// @startlock
+	{// @endlock
+		debugger;
+		changed = false;
+		if($$(getHtmlId("attName")).sourceAtt.getValue() != currentName)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attFullName")).sourceAtt.getValue() != currentDisplay)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attPhone")).sourceAtt.getValue() != currentPhone)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attExt")).sourceAtt.getValue() != currentExt)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attAdditional")).sourceAtt.getValue() != currentMobile)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attFax")).sourceAtt.getValue() != currentFax)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attAddress")).sourceAtt.getValue() != currentAddress)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attState")).sourceAtt.getValue() != currentState)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attZip")).sourceAtt.getValue() != currentZip)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attEmail")).sourceAtt.getValue() != currentEmail)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attNotes")).sourceAtt.getValue() != currentNotes)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attCityComboBox")).getValue() != currentCity)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attReportingComboBox")).getValue() != currentReportingMethod)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attInactive")).getValue() != sources.specificAssessorList.InactiveAssessorInfo)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attPublicDef")).getValue() != currentDefender)
+		{
+			changed = true;
+		}
+		if($$(getHtmlId("attStaffInfo")).sourceAtt.getValue() != currentStafInfo)
+		{
+			changed = true;
+		}
+		
+		//run update if needed
+		if(changed)
+		{
+			var myObject7 = 
+			{
+				token:'7836140170460568' ,id:'1',major:3,minor:3,
+				data1:$$(getHtmlId("attName")).sourceAtt.getValue(),
+				data2:$$(getHtmlId("attPhone")).sourceAtt.getValue(),
+				data3:$$(getHtmlId("attEmail")).sourceAtt.getValue(),
+				data4:$$(getHtmlId("attFax")).sourceAtt.getValue(),
+				data5:$$(getHtmlId("attAddress")).sourceAtt.getValue(),
+				data6:$$(getHtmlId("attCityComboBox")).getValue(),
+				data7:$$(getHtmlId("attState")).sourceAtt.getValue(),
+				data8:$$(getHtmlId("attZip")).sourceAtt.getValue(),
+				data9:$$(getHtmlId("attExt")).sourceAtt.getValue(),
+				data10:$$(getHtmlId("attAdditional")).sourceAtt.getValue(),
+				data11:$$(getHtmlId("attNotes")).sourceAtt.getValue(),
+				data12:$$(getHtmlId("attFullName")).sourceAtt.getValue(),
+				data13:$$(getHtmlId("attReportingComboBox")).getValue(),
+				data14:currentID,
+				data15:$$(getHtmlId("attInactive")).getValue(),
+				data16:$$(getHtmlId("attPublicDef")).getValue(),
+				data17:$$(getHtmlId("attStaffInfo")).getValue()
+				
+				
+			}; //dontforget to add this to token userConfigObj.secToken  userConfigObj.userID
+	 		assessorUpdate = rpcDUpdate.setUpdate(myObject7);
+	 		bakAssessorInsert = rpcDInsert.setInsert(myObject7);
+	 		
+		}
+	};// @lock
+
+	button15.click = function button15_click (event)// @startlock
+	{// @endlock
+		$$(getHtmlId('mainAttorneyCont')).setSplitPosition(1290);
+	};// @lock
+
+	button3.click = function button3_click (event)// @startlock
+	{// @endlock
+		$$(getHtmlId('attSpecificInfoCont')).setSplitPosition(900);
+	};// @lock
+
+	Close.click = function Close_click (event)// @startlock
+	{// @endlock
+		$$(getHtmlId('attPrevVersionCont')).setSplitPosition(625);
+	};// @lock
+
+	dataGrid1.onRowClick = function dataGrid1_onRowClick (event)// @startlock
+	{// @endlock
+		$$(getHtmlId('attPrevVersionCont')).setSplitPosition(250);
+		var date = event.data.row.cells[0].value;
+		var assessor = event.data.row.cells[1].value;
+		var employee = event.data.row.cells[2].value;
+		
+		var myObject6 = {token:'7836140170460568' ,id:'1',major:3,minor:19,data1:assessor, data2:date, data3:employee}; //dontforget to add this to token userConfigObj.secToken  userConfigObj.userID
+	 	specificBakAttorneyList = rpcDSelects.getSelect(myObject6);
+	 	sources.specificBakAttorneyList.sync();
+	};// @lock
+
+	button16.click = function button16_click (event)// @startlock
+	{// @endlock
+		$$(getHtmlId('attSpecificInfoCont')).setSplitPosition(420);
+		//var name = $$(getHtmlId('assessorNameField')).getValue();
+		var myObject5 = {token:'7836140170460568' ,id:'1',major:3,minor:18,data1:currentID}; //dontforget to add this to token userConfigObj.secToken  userConfigObj.userID
+	 	bakAttorneyInfo = rpcDSelects.getSelect(myObject5);
+	 	sources.bakAttorneyInfo.sync();
+	};// @lock
+
 	attVoidedCorrespondanceCheck.change = function attVoidedCorrespondanceCheck_change (event)// @startlock
 	{// @endlock
 		var status = $$(getHtmlId("attVoidedCorrespondanceCheck")).getValue();
 		var id  = $$(getHtmlId('attHiddenCorrId')).getValue();
 		var myObject8 = 
 			{
-				token:'7836140170460568' ,id:'1',major:3,minor:1,
+				token:'7836140170460568' ,id:'1',major:3,minor:2,
 				data1:currentCorresondanceDate,
 				data2:status,
 				data3: id
 			}; //dontf
 		assessorUpdate = rpcDUpdate.setUpdate(myObject8);
+		
+		
 	};// @lock
 
 	button2.click = function button2_click (event)// @startlock
@@ -160,6 +305,7 @@ function constructor (id) {
 			}; //dontf
 			var update = rpcDInsert.setInsert(myObject8);
 			fillCorrespondance();
+			x.setValue("");
 			/*
 			var myObject2 = {token:'7836140170460568' ,id:'1',major:3,minor:2,data1:currentName}; //dontforget to add this to token userConfigObj.secToken  userConfigObj.userID
 	 			assessorCorespondance = rpcDSelects.getSelect(myObject2);
@@ -229,21 +375,23 @@ function constructor (id) {
 		
 		
 		///////////////////////////////////////////////////////////get data
-		
+		debugger;
 		currentName = name;
 		currentCity = city;
 		currentReportingMethod = reportingMethod;
-		currentPhone = sources.specificAttorneyList.AssessorPhone;
-		currentExt = sources.specificAttorneyList.AssessorPhoneExt;
-		currentEmail = sources.specificAttorneyList.AssessorEmail;
-		currentFax = sources.specificAttorneyList.AssessorFax;
-		currentMobile = sources.specificAttorneyList.AssessorMobilePhone;
-		currentAddress = sources.specificAttorneyList.AssessorAddress;
-		currentState = sources.specificAttorneyList.AssessorState;
-		currentZip = sources.specificAttorneyList.AssessorZipCode;
-		currentNotes = sources.specificAttorneyList.AssessorNotes;
-		currentDisplay = sources.specificAttorneyList.AssessorFullNameDisplay;
-		currentID = sources.specificAttorneyList.AssessorInformationID;
+		currentPhone = sources.specificAttorneyList.AttorneyPhone;
+		currentExt = sources.specificAttorneyList.AttorneyPhoneExt;
+		currentEmail = sources.specificAttorneyList.AttorneyEmail;
+		currentFax = sources.specificAttorneyList.AttorneyFax;
+		currentMobile = sources.specificAttorneyList.AttorneyAdditionalPhone;
+		currentAddress = sources.specificAttorneyList.AttorneyAddress;
+		currentState = sources.specificAttorneyList.AttorneyState;
+		currentZip = sources.specificAttorneyList.AttorneyZipCode;
+		currentNotes = sources.specificAttorneyList.AttorneyNotes;
+		currentDisplay = sources.specificAttorneyList.AttorneyFullNameDisplay;
+		currentID = sources.specificAttorneyList.AttorneyInformationID;
+		currentDefender = sources.specificAttorneyList.PublicDefender;
+		currentStafInfo = sources.specificAttorneyList.AttorneyStaffInfo;
 		
 		if(currentName == null)
 		{
@@ -315,6 +463,12 @@ function constructor (id) {
 	};// @lock
 
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_button14", "click", button14.click, "WAF");
+	WAF.addListener(this.id + "_button15", "click", button15.click, "WAF");
+	WAF.addListener(this.id + "_button3", "click", button3.click, "WAF");
+	WAF.addListener(this.id + "_Close", "click", Close.click, "WAF");
+	WAF.addListener(this.id + "_dataGrid1", "onRowClick", dataGrid1.onRowClick, "WAF");
+	WAF.addListener(this.id + "_button16", "click", button16.click, "WAF");
 	WAF.addListener(this.id + "_attVoidedCorrespondanceCheck", "change", attVoidedCorrespondanceCheck.change, "WAF");
 	WAF.addListener(this.id + "_button2", "click", button2.click, "WAF");
 	WAF.addListener(this.id + "_attCorCloseButton", "click", attCorCloseButton.click, "WAF");
