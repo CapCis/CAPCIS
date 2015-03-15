aaa73c3da4c95720626a10f2069db4205428a8e0
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-03-14 19:56:58 -0500

    Update #367

---
+++ CAPCIS/bootStraps/httpReportHandler.js
@@ -0,0 +1,5 @@
+﻿addHttpRequestHandler(
+      '/makeMyReportBitch',               // (string) regex used to filter the requests to be intercepted
+      'C:/Users/Tim/Documents/Wakanda/CAPCIS/CAPCIS/Modules/testhttpresponse.js',  // (string) name of the file where the request handler function is specified
+      'getMyReport'         // (string) name of the request handler function
+);
