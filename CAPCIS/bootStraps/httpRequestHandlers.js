109891af40b7aaf393943a88d895e15eb6692494
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-03-16 21:55:56 -0500

    Merge branch 'TLCAPCIS'
    Conflicts:
    	CAPCIS/DataFolder/data.WakTDef

--- CAPCIS/bootStraps/httpRequestHandlers.js
+++ CAPCIS/bootStraps/httpRequestHandlers.js
@@ -1,6 +1,6 @@
 ﻿addHttpRequestHandler(
       '/makeMyReportBitch',               // (string) regex used to filter the requests to be intercepted
-      'C:/Users/Tim/Documents/Wakanda/CAPCIS/CAPCIS/Modules/testhttpresponse.js',  // (string) name of the file where the request handler function is specified
+      'C:/Users/Tim/Documents/Wakanda/CAPCIS/CAPCIS/Modules/httpRequestHandlerResponses.js',  // (string) name of the file where the request handler function is specified
       'getMyReport'         // (string) name of the request handler function
 );
 
