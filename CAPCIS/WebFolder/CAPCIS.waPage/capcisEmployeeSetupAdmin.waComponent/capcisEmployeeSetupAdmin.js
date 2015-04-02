f345dab12f31a2396cffb892d8e6aad514e6f065
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-04-01 21:40:30 -0500

    Update #499

--- CAPCIS/WebFolder/CAPCIS.waPage/capcisEmployeeSetupAdmin.waComponent/capcisEmployeeSetupAdmin.js
+++ CAPCIS/WebFolder/CAPCIS.waPage/capcisEmployeeSetupAdmin.waComponent/capcisEmployeeSetupAdmin.js
@@ -13,11 +13,31 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var button2 = {};	// @button
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+		
+	};// @lock
+
+
+	var myComboObject = {token:userConfigObj.secToken ,id:userConfigObj.userID,major:0,minor:6};
+	rpcDSelects.getSelectAsync({
+		 			'onSuccess': function(result){
+		 				debugger;
+		 				adminDivCombo = result;
+		 				sources.adminDivCombo.sync();
+					},
+					'onError': function(error){
+						alert(error);
+					},
+					'params': [myComboObject]
+	});	
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_button2", "click", button2.click, "WAF");
 	// @endregion// @endlock
 
 	};// @lock
